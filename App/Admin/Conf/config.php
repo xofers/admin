fc845f2adf90d96f6f9584edfbdfd53cda279f46
MAX <1518140867@qq.com>
2015-02-02 10:31:23 +0800

    修复以前版本多删除的文件

--- App/Admin/Conf/config.php
+++ App/Admin/Conf/config.php
@@ -11,11 +11,11 @@ return array(
     /* 模板相关配置 */
     'TMPL_PARSE_STRING' => array(
         '__STATIC__'   => __ROOT__ . '/Public/Static',
-   		'__SCRIPTS__'  => __ROOT__.'/Public/' . MODULE_NAME . '/scripts',
-   		'__ADDONS__'   => __ROOT__.'/Public/' . MODULE_NAME . '/Addons',
-        '__IMG__'      => __ROOT__.'/Public/' . MODULE_NAME . '/images',
-        '__CSS__'      => __ROOT__.'/Public/' . MODULE_NAME . '/css',
-        '__JS__'       => __ROOT__.'/Public/' . MODULE_NAME . '/js',
+   		'__SCRIPTS__'  => __ROOT__ .'/Public/' . MODULE_NAME . '/scripts',
+   		'__ADDONS__'   => __ROOT__ .'/Public/' . MODULE_NAME . '/Addons',
+        '__IMG__'      => __ROOT__ .'/Public/' . MODULE_NAME . '/images',
+        '__CSS__'      => __ROOT__ .'/Public/' . MODULE_NAME . '/css',
+        '__JS__'       => __ROOT__ .'/Public/' . MODULE_NAME . '/js',
     ),
     
 	/* SESSION 和 COOKIE 配置 */
--- App/Home/Controller/HomeController.class.php
+++ App/Home/Controller/HomeController.class.php
@@ -19,7 +19,7 @@ class HomeController extends Controller {
         $config = api('Config/lists');
 		
         C($config); //添加配置
-		        
+        
         if(!C('WEB_SITE_CLOSE')){
             $this->error('站点已经关闭，请稍后访问~');
         }
